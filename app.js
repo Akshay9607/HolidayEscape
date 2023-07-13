fdc9de5d11b3bb9f4cc52a920466c9ec1e106e61
Akshay9607 <akshay96070418@gmail.com>
2023-07-14 00:46:20 +0530

    Modified policy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-	origin: 'http://localhost:3000',
+	origin: 'https://effortless-bunny-8856a6.netlify.app',
 	credentials: true, //access-control-allow-credentials:true
 	optionSuccessStatus: 200,
 };
